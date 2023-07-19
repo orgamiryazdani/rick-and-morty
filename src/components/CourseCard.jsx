ae5488a2e823f10fcc0ec40905b4cbe73f6cca3f
Saheb Mohamadi <saheb.put92@gmail.com>
2023-07-20 00:56:27 +0330

    extract components

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,36 +1,55 @@
-function CourseCard() {
+function CourseCard({ course }) {
   return (
     <div className="course-item">
       <div className="course-item__img">
-        <img src="/images/img1.jpg" alt="image-1" />
+        <img src={course.imageUrl} alt={course.title} />
       </div>
       <div className="course-item__detail">
-        <div className="course-item__body">
-          <div>
-            <p className="title">React.js Course </p>
-            <p className="desc"> The Ultimate React and Redux Course</p>
-          </div>
-          <span className="rate">4</span>
-        </div>
-        <div className="course-item__footer">
-          <div className="tags">
-            <span className="badge badge--secondary">React.js</span>
-            <span className="badge badge--secondary"> Fontend</span>
-          </div>
-          <div className="caption">
-            <div className="date">
-              {new Date().toLocaleDateString("en-US", {
-                month: "short",
-                year: "numeric",
-                day: "numeric",
-              })}
-            </div>
-            <span className="badge badge--primary">Completed</span>
-          </div>
-        </div>
+        <CourseCardBody
+          description={course.description}
+          title={course.title}
+          rate={course.rate}
+        />
+        <CourseCardFooter course={course} />
       </div>
     </div>
   );
 }
 
 export default CourseCard;
+
+function CourseCardBody({ title, description, rate }) {
+  return (
+    <div className="course-item__body">
+      <div>
+        <p className="title">{title}</p>
+        <p className="desc"> {description}</p>
+      </div>
+      <span className="rate"> {rate} </span>
+    </div>
+  );
+}
+
+function CourseCardFooter({ course }) {
+  return (
+    <div className="course-item__footer">
+      <div className="tags">
+        {course.tags.map((t) => (
+          <span key={t} className="badge badge--secondary">
+            React.js
+          </span>
+        ))}
+      </div>
+      <div className="caption">
+        <div className="date">
+          {new Date(course.start).toLocaleDateString("en-US", {
+            month: "short",
+            year: "numeric",
+            day: "numeric",
+          })}
+        </div>
+        <span className="badge badge--primary">{course.status}</span>
+      </div>
+    </div>
+  );
+}
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -5,10 +5,10 @@ const courses = [
     id: 1,
     title: "English Lecture",
     description: "language lessons with the most popular teachers",
-    imagUrl: "/img1.jpg",
+    imageUrl: "/images/img1.jpg",
     rate: "4.5",
     tags: ["langauges"],
-    start: "2023-07-01T20:46:30.615Z",
+    start: "2021-08-01T20:46:30.615Z",
     status: "Completed",
   },
   {
@@ -16,7 +16,7 @@ const courses = [
     title: "Design Strategy",
     description:
       "lesson on planning design concept and proper planning of work",
-    imagUrl: "/img2.jpg",
+    imageUrl: "/images/img2.jpg",
     rate: "4",
     tags: ["UI/UX design", "web design"],
     start: "2023-07-01T20:46:30.615Z",
@@ -27,7 +27,7 @@ const courses = [
     title: "Business Lecture",
     description:
       "lectures on how to build your buisness safely without fare of new projects",
-    imagUrl: "/img3.jpg",
+    imageUrl: "/images/img3.jpg",
     rate: "3.9",
     tags: ["Marketing", "Finance"],
     start: "2023-07-01T20:46:30.615Z",
@@ -38,9 +38,9 @@ const courses = [
 function CourseList() {
   return (
     <div className="course-list">
-      {courses.map((course) => {
-        return <CourseCard key={course.id} />;
-      })}
+      {courses.map((course) => (
+        <CourseCard key={course.id} course={course} />
+      ))}
     </div>
   );
 }
